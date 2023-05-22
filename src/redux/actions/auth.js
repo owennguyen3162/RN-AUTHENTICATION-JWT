8c5f12203f359a91d9732c0c56cb57428dbb0608
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-23 00:58:34 +0700

    feature: action-auth

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,49 @@
+import { LOGIN, REFRESH_TOKEN, REGISTER,GET_INFO } from "./types";
+
+import AuthService from "../services/auth.service";
+import { navigate } from "../../../components/navigation/RootNavigation";
+
+export const register = (username, password) => async (dispatch) => {
+  try {
+    const res = await AuthService.register(username, password);
+    if (res.status === 201) {
+      // dispatch({ payload: res, type: REGISTER, status: res.status });
+      alert("REGISTER DONE");
+    }
+  } catch (error) {
+    alert("Register fail :::" + error);
+  }
+};
+
+export const login = (username, password) => async (dispatch) => {
+  try {
+    const res = await AuthService.login(username, password);
+    if (res) {
+      dispatch({ payload: res, type: LOGIN });
+      navigate("Home");
+    }
+  } catch (error) {
+    alert("LOGIN FAIL :::" + error);
+  }
+};
+
+export const getInfo = () => async (dispatch) => {
+  try {
+    const res = await AuthService.getInfo();
+    if (res) {
+      // console.log("DA VAO DAYYYYYYYYYYYYYYYy"+JSON.stringify(res.data));
+      dispatch({ payload: res, type: GET_INFO });
+    }
+  } catch (error) {
+    alert("GET INFO FAIL:::" + error);
+  }
+};
+
+export const logout = {};
+
+export const refreshToken = (accessToken) => (dispatch) => {
+  dispatch({
+    type: REFRESH_TOKEN,
+    payload: accessToken,
+  });
+};
