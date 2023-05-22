81afe0750cd44846aa5b591faa799a59c683ec52
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-23 00:56:41 +0700

    feature: set-up App.js

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./components/Screen/Login";
+import Register from "./components/Screen/Register";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
+import setup from "./src/redux/services/setupInterceptors";
+import Home from "./components/Screen/Home";
+import { navigationRef } from "./components/navigation/RootNavigation"
+const Stack = createNativeStackNavigator();
+const App = () => {
+  setup(store);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen component={Login} name="Login" />
+          <Stack.Screen component={Register} name="Register" />
+          <Stack.Screen component={Home} name="Home" />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
