cfebe08ea929883e35591b9afc35930cd42bc9be
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-24 15:41:25 +0700

    Update auth.js

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,47 +1,68 @@
-import { LOGIN, REFRESH_TOKEN, REGISTER, GET_INFO } from "./types";
+import { FETCH_REQUEST, FETCH_SUCCESS, FETCH_FAIL } from "./types";
 
 import AuthService from "../services/auth.service";
 import { navigate } from "../../components/navigation/RootNavigation";
 
+const REQUEST = () => {
+  return {
+    payload: null,
+    isLoading: true,
+    type: FETCH_REQUEST,
+  };
+};
+const SUCCESS = (data) => {
+  return {
+    payload: data,
+    isLoading: false,
+    type: FETCH_SUCCESS,
+  };
+};
+
+const FAIL = () => {
+  return {
+    payload: null,
+    isLoading: false,
+    type: FETCH_FAIL,
+  };
+};
 export const register = (username, password) => async (dispatch) => {
+  dispatch(REQUEST());
   try {
     const res = await AuthService.register(username, password);
     if (res.status === 201) {
-      alert("REGISTER DONE");
+      dispatch(SUCCESS(res));
+      alert("register successfully");
     }
   } catch (error) {
-    alert("Register fail :::" + error);
+    dispatch(FAIL());
+    alert("register fail");
   }
 };
 
 export const login = (username, password) => async (dispatch) => {
+  dispatch(REQUEST());
   try {
     const res = await AuthService.login(username, password);
     if (res) {
-      dispatch({ payload: res, type: LOGIN });
+      dispatch(SUCCESS(res));
       navigate("Home");
     }
   } catch (error) {
-    alert("LOGIN FAIL :::" + error);
+    dispatch(FAIL());
+    alert("Login fail");
   }
 };
 
 export const getInfo = () => async (dispatch) => {
+  dispatch(REQUEST());
   try {
     const res = await AuthService.getInfo();
-    if (res) {
-      dispatch({ payload: res, type: GET_INFO });
+    console.log(res);
+    if (res.status === 200) {
+      dispatch(SUCCESS(res));
     }
   } catch (error) {
-    alert("GET INFO FAIL:::" + error);
+    dispatch(FAIL());
+    alert("get info fail");
   }
-};
-
-export const logout = {};
-
-export const refreshToken = (accessToken) => (dispatch) => {
-  dispatch({
-    type: REFRESH_TOKEN,
-    payload: accessToken,
-  });
 };
