630830622b331e15101f66e0fe16fafc6f79e7be
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-24 12:39:24 +0700

    Update auth.js

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,4 +1,4 @@
-import { LOGIN, REFRESH_TOKEN, REGISTER,GET_INFO } from "./types";
+import { LOGIN, REFRESH_TOKEN, REGISTER, GET_INFO } from "./types";
 
 import AuthService from "../services/auth.service";
 import { navigate } from "../../components/navigation/RootNavigation";
@@ -7,7 +7,6 @@ export const register = (username, password) => async (dispatch) => {
   try {
     const res = await AuthService.register(username, password);
     if (res.status === 201) {
-      // dispatch({ payload: res, type: REGISTER, status: res.status });
       alert("REGISTER DONE");
     }
   } catch (error) {
@@ -31,7 +30,6 @@ export const getInfo = () => async (dispatch) => {
   try {
     const res = await AuthService.getInfo();
     if (res) {
-      // console.log("DA VAO DAYYYYYYYYYYYYYYYy"+JSON.stringify(res.data));
       dispatch({ payload: res, type: GET_INFO });
     }
   } catch (error) {
