7f2b7ee782f64b3f87f12a8621dd78df0b943faf
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-24 22:48:44 +0700

    Update types.js

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,3 +1,6 @@
 export const FETCH_REQUEST = "FETCH_REQUEST";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAIL = "FETCH_FAIL";
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+
