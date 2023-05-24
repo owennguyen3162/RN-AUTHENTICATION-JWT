914983ae74ab477e164ef6c753358d187f075b09
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-24 22:48:38 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { store } from "./src/redux/store";
 import { navigationRef } from "./src/components/navigation/RootNavigation";
 import Login from "./src/components/Screen/Login";
@@ -11,55 +11,47 @@ import Home from "./src/components/Screen/Home";
 import TokenService from "./src/redux/services/token.service";
 const Stack = createNativeStackNavigator();
 const App = () => {
-  const [user, setUser] = React.useState(undefined);
-  const [Loading, setLoading] = React.useState(true);
-
-  const settingOptions = {
-    headerShown: false,
-  };
-  React.useEffect(() => {
-    getCurrent();
-  }, []);
-
-  const getCurrent = async () => {
-    try {
-      const value = await TokenService.getUser();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-  return Loading ? (
-    <ActivityIndicator />
-  ) : (
+  return (
     <Provider store={store}>
       <NavigationContainer ref={navigationRef}>
-        <Stack.Navigator screenOptions={settingOptions}>
-          {user === undefined
-            ? (() => AuthNavigator())()
-            : (() => MainNavigator())()}
-        </Stack.Navigator>
+        <MainNavigator />
       </NavigationContainer>
     </Provider>
   );
 };
 
-const MainNavigator = () => {
+const MainNavigator = ({ navigation }) => {
+  const settingOptions = {
+    headerShown: false,
+  };
+
+  const data = useSelector((value) => value.isSignedIn);
+  const [User, setUser] = React.useState("");
+
+  React.useEffect(() => {
+    TokenService.getUser().then((res) => setUser(res));
+  }, []);
+
   return (
-    <>
-      <Stack.Screen component={Home} name="Home" />
-    </>
+    <Stack.Navigator screenOptions={settingOptions}>
+      {data.isSignedIn || User !== undefined ? (
+        <Stack.Screen component={Home} name="Home" />
+      ) : (
+        <>
+          <Stack.Screen component={Login} name="Login" />
+          <Stack.Screen component={Register} name="Register" />
+        </>
+      )}
+    </Stack.Navigator>
   );
 };
 
-const AuthNavigator = () => {
-  return (
-    <>
-      <Stack.Screen component={Login} name="Login" />
-      <Stack.Screen component={Register} name="Register" />
-      <Stack.Screen component={Home} name="Home" />
-    </>
-  );
-};
+// const AuthNavigator = () => {
+//   return (
+//     <>
+//       <Stack.Screen component={Login} name="Login" />
+//       <Stack.Screen component={Register} name="Register" />
+//     </>
+//   );
+// };
 export default App;
