7b23e23cf74fe4c7f5e2e78fedddc0e2c4131cf4
owennguyen3162 <nguyentuan3162@gmail.com>
2023-05-26 16:47:07 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider, useSelector, useDispatch } from "react-redux";
